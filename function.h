16b7a2f16c7ca82d2a4341b92a1fb06ea05fe4a7
吉田大輝 <bob181219@yahoo.co.jp>
2018-04-14 16:16:28 +0900

    Describe declarations of tanzanite::CommandLine function and tanzanite::interpret function

---
+++ function.h
@@ -0,0 +1,30 @@
+// Copyright 2018 Daiki Yoshida. All rights reserved.
+//
+// This file is part of Tanzanite.
+//
+// Tanzanite is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Tanzanite is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU Generaal Public License
+// along with Tanzanite. If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+// standard library
+#include <string>
+
+namespace tanzanite
+{
+	void CommandLine();
+	void interpret(const std::wstring& filename);
+}
+
+#endif
